93a053d6ac824cd90c79c43a5e894e065b8e738f
Paul Yuan <Paul@Pauls-MacBook-Pro.local>
2018-07-30 00:13:27 -0700

    basic calc v1

---
+++ Meal CalculatorTests/Meal_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meal_CalculatorTests.swift
+//  Meal CalculatorTests
+//
+//  Created by Paul Yuan on 7/30/18.
+//  Copyright © 2018 Paul Yuan. All rights reserved.
+//
+
+import XCTest
+@testable import Meal_Calculator
+
+class Meal_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
